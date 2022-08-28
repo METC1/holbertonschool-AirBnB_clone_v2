f5b0b4db36adc3b4428a5987e5200937ddda346f
METC <metc_ck1@hotmail.com>
2022-08-28 15:46:09 -0700

    0-setup_web_static fix 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/shared/
 chown -R ubuntu:ubuntu /data/
 echo "Holberton School - test /data/web_static/.../index.html"  > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
