52ce7e53de8f38a7aee0794d3a260cb3c8431a76
METC <metc_ck1@hotmail.com>
2022-08-28 16:09:39 -0700

    0-setup_web_static fix 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,9 +6,8 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-chown -R ubuntu:ubuntu /data/
 echo "Holberton School - test /data/web_static/.../index.html"  > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sudo sed -i '+server_name_;+a \n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+chown -hR ubuntu:ubuntu /data/
+sudo sed -i '/server_name_;/a \n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
