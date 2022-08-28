fd2587564960db4c2aea09908eb34bb3ca46e986
METC <metc_ck1@hotmail.com>
2022-08-28 16:45:25 -0700

    0-setup_web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,4 +10,5 @@ echo "Holberton School - test /data/web_static/.../index.html"  > /data/web_stat
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i '/server_name _;/a \n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/server_name _;/a \n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
