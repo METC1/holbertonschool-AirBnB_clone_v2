3e45d5867fe6d8e6e85311e928863bfc6fda2589
METC <metc_ck1@hotmail.com>
2022-08-28 15:09:52 -0700

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Script that sets up webserver for the deployment of web_static
+
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+cat > /data/web_static/releases/test/index.html <<'EOF'
+<html>
+<head>
+</head>
+<body>
+    <h1>Holberton Scholl - test /data/web_static/releases/test/index.html</h1>
+</body>
+</html>  
+EOF
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sudo sed -i "+server_name_;+a \n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+service nginx restart
