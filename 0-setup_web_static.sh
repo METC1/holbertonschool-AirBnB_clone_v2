39d838a5371a4343e165b4b69e809391bc02af8e
METC <metc_ck1@hotmail.com>
2022-08-28 17:25:11 -0700

    0-setup_web_static fin

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,9 @@
 
 apt-get update
 apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School - test /data/web_static/.../index.html"  > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test /data/web_static/shared/
+echo " Holberton School - Web_static test " > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i "/listen 80 default_server/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sed -i "/listen 80 default_server/a \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 service nginx restart
