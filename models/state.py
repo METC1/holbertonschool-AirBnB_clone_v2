183b7b3d50ca44c535bc1579761e566f25b97fe8
METC <metc_ck1@hotmail.com>
2022-09-13 12:17:44 -0700

    task 7 again

--- models/state.py
+++ models/state.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from os import getenv, environ
 from models.city import City
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     '''
     class State that inherets from BaseModel
     '''
-    name=""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
-    @property
-    def cities(self):
-        from models import storage
-        all_cities = storage.all(City).values()
+    if (getenv('HBN_TYPE_STORAGE') == 'db'):
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            all_cities = storage.all(City).values()
 
-        new_list = []
-        for city in all_cities:
-            if (city.state_id == self.id):
-                new_list.append(city)
-        return (new_list)
+            new_list = []
+            for city in all_cities:
+                if (city.state_id == self.id):
+                    new_list.append(city)
+            return new_list
