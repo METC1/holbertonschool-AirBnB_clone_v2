d70ca09b396a118b804f6d9445dd258ba09140de
METC <metc_ck1@hotmail.com>
2022-09-13 12:26:55 -0700

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,7 +6,7 @@ Task 6
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.amenity import Amenity
 from models.city import City
 from models.state import State
