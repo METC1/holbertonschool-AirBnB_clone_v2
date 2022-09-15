a088d384a7fe47c017f6d74c4a2a55a82dbb5851
METC <metc_ck1@hotmail.com>
2022-09-14 17:35:00 -0700

    7-states_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@
 """
 from flask import Flask, render_template
 from models import storage
-app =Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -15,9 +15,11 @@ def list_states():
     states = storage.all("State").values()
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def close_db(db):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
